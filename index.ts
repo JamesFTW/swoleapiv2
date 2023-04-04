14303a657659839e6f63fbb04b61a53e6aefbb7f
James Andrews <bdaddyja@gmail.com>
2023-04-03 19:48:00 -0700

    Hello world express on vercel

--- index.ts
+++ index.ts
@@ -1,36 +1,70 @@
 import { PrismaClient } from '@prisma/client'
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const prisma = new PrismaClient()
+const app: Express = express();
+const port = process.env.PORT;
 
-async function main() {
-    await prisma.user.create({
-      data: {
-        name: 'Alice',
-        email: 'alice@prisma.io',
-        posts: {
-          create: { title: 'Hello World' },
-        },
-        profile: {
-          create: { bio: 'I like turtles' },
-        },
-      },
-    })
-  
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
+
+app.get('/about', async function (req: Request, res: Response) {
+  try {
     const allUsers = await prisma.user.findMany({
       include: {
         posts: true,
         profile: true,
       },
     })
-    console.dir(allUsers, { depth: null })
+    res.json(allUsers)
+  } catch (e) {
+    res.status(500).json({
+      message: "Something went wrong",
+      error: e
+    })
   }
+})
 
-main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+// async function main() {
+//     await prisma.user.create({
+//       data: {
+//         name: 'Alice',
+//         email: 'alice@prisma.io',
+//         posts: {
+//           create: { title: 'Hello World' },
+//         },
+//         profile: {
+//           create: { bio: 'I like turtles' },
+//         },
+//       },
+//     })
+  
+//     const allUsers = await prisma.user.findMany({
+//       include: {
+//         posts: true,
+//         profile: true,
+//       },
+//     })
+//     console.dir(allUsers, { depth: null })
+//   }
+
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect()
+//   })
+//   .catch(async (e) => {
+//     console.error(e)
+//     await prisma.$disconnect()
+//     process.exit(1)
+//   })
+
+
+module.exports = app
