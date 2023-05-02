61fb4464b9eb249ce40df134539e77a20b1d8f45
James Andrews <bdaddyja@gmail.com>
2023-05-02 14:58:12 -0700

    Add Exercises table to db

---
+++ api/exercises/models/index.js
@@ -0,0 +1,16 @@
+//This is where queries to the database happens.
+/**
+  Class Exercise {
+   getAllExercises() {
+    return all exercises;
+  }
+  
+  }
+
+
+  useage:
+
+  router.get("/exercise", () => {
+    Exercise.getAllExercises()
+  })
+ */
---
+++ api/exercises/services/index.js
@@ -0,0 +1,2 @@
+//This is where the functions that call that will be used by endpoints go.
+// Almost like a middleman between the models and the endpoints
