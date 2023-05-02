8307bd58a1fa53526de23944480456d66a049ec2
James Andrews <bdaddyja@gmail.com>
2023-05-02 14:23:06 -0700

    Use helmet

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Express } from 'express'
 import dotenv from 'dotenv'
 import api from './api'
+import helmet from "helmet";
 
 dotenv.config()
 
@@ -8,6 +9,7 @@ const app: Express = express()
 const port = process.env.PORT
 
 app.use(express.urlencoded())
+app.use(helmet())
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
