fdb7da34fa6370825a6a4c3d2831d999967d45b8
James Andrews <bdaddyja@gmail.com>
2023-05-08 19:59:25 -0700

    Add getByUserName and getByUserid

--- api/users/models/index.ts
+++ api/users/models/index.ts
@@ -25,8 +25,37 @@ export class Users  {
             lastName,
             email,
             password,
+            salt
           }
         })
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  async getByUserName(userName: string) {
+    try {
+      const user = await prisma.users.findUnique({
+        where: {
+          userName: userName
+        }
+      })
+
+      return user
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  async getByUserid(userId: string) {
+    try {
+      const user = await prisma.users.findUnique({
+        where: {
+          userId: userId
+        }
+      })
+
+      return user
     } catch (e) {
       console.log(e)
     }
