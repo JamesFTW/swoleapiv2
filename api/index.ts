b4ca06f07fd9a61cef72699cd456c7b17d5b4d8f
James Andrews <bdaddyja@gmail.com>
2023-06-02 13:09:06 -0700

    Add routes index

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,9 @@
 import express from 'express'
-import usersEndpoints from './users/endpoints'
-import exercisesEndpoints from './exercises/endpoints'
+import usersRoutes from './users/routes'
+import exercisesRoutes from './exercises/routes'
 
 const router  = express.Router()
-router.use('/users', usersEndpoints)
-router.use('/exercises', exercisesEndpoints)
+router.use('/users', usersRoutes)
+router.use('/exercises', exercisesRoutes)
 
 export default router
