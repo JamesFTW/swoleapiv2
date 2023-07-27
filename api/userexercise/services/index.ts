4a4b42f649cf781e89405fcb60b990e976e5cacc
James Andrews <bdaddyja@gmail.com>
2023-07-27 16:59:46 -0700

    Remove unused import

--- api/userexercise/services/index.ts
+++ api/userexercise/services/index.ts
@@ -1,5 +1,5 @@
 import { UserExercisesModel } from '../models'
-import { PrismaClient, UserExercises } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export class UserExercisesServices {
