c735f03d6ebef8b824df0ff7d71fdfe96fa4be48
James Andrews <bdaddyja@gmail.com>
2023-08-01 12:13:54 -0700

    Use transaction for user exercises

--- api/userexercise/services/index.ts
+++ api/userexercise/services/index.ts
@@ -15,44 +15,51 @@ export class UserExercisesServices {
     weightMoved: number,
     reps: number
   ): Promise<void> {
-
-    const userExists = await prisma.users.findUnique({
-      where: {
-        userId: userId,
-      },
-    })
-
-    if (!userExists) {
-      return Promise.reject('User with the provided userId does not exist.')
-    }
-
-    // @ts-ignore
-    const exerciseIdInt = parseInt(exerciseId)
-
-    const exerciseExists = await prisma.exercises.findUnique({
-      where: {
-        exerciseId: exerciseIdInt,
-      },
-    })
-
-    if (!exerciseExists) {
-      return Promise.reject('Exercise with the provided exerciseId does not exist.')
-    }
-
-    // @ts-ignore
-    const weightMovedInt = parseInt(weightMoved)
-
-    // @ts-ignore
-    const repsInt = parseInt(reps)
-
     try {
-      await this.userExercise.create(
-        exerciseIdInt,
-        userId,
-        weightMovedInt,
-        repsInt
-      )
-    } catch(error) {
+      await prisma.$transaction(async (prisma) => {
+        const userExists = await prisma.users.findUnique({
+          where: {
+            userId: userId,
+          },
+        })
+
+        if (!userExists) {
+          throw new Error('User with the provided userId does not exist.')
+        }
+
+         // @ts-ignore
+        const exerciseIdInt = parseInt(exerciseId)
+
+        const exerciseExists = await prisma.exercises.findUnique({
+          where: {
+            exerciseId: exerciseIdInt,
+          },
+        })
+
+        if (!exerciseExists) {
+          throw new Error('Exercise with the provided exerciseId does not exist.')
+        }
+
+         // @ts-ignore
+        const weightMovedInt = parseInt(weightMoved)
+
+         // @ts-ignore
+        const repsInt = parseInt(reps)
+
+        await prisma.userExercises.create({
+          data: {
+            exercise: {
+              connect: { exerciseId: exerciseIdInt },
+            },
+            user: {
+              connect: { userId: userId },
+            },
+            weightMoved: weightMovedInt,
+            reps: repsInt,
+          },
+        })
+      })
+    } catch (error) {
       return Promise.reject(error)
     }
   }
