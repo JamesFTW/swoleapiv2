7b37efd476011c8e195ee44141ef59b8825cea72
James Andrews <bdaddyja@gmail.com>
2024-05-23 13:06:22 -0700

    [Issue-34][Issue-36] Implement Server-side Logic for Daily Workout Data

--- api/workouts/routes/workoutRoutes.ts
+++ api/workouts/routes/workoutRoutes.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from 'express'
 import { authenticate } from '@middleware/authenticate'
 import { WeeklySnapshotsService } from '../snapshots/services/WeeklySnapshotsService'
+import { CompletedWorkoutsService } from '../data/completedworkouts/services/completedWorkoutsService'
 
 const router = express.Router()
-const weeklySnapshot = new WeeklySnapshotsService()
 
-router.post('/createSnapshot', authenticate, async (req: Request, res: Response) => {
+router.post('/createOrUpdateSnapshot', authenticate, async (req: Request, res: Response) => {
+  const weeklySnapshot = new WeeklySnapshotsService()
+
   try {
     const { userId } = req.session?.passport?.user
     const { numberOfSets, totalVolume, totalWorkoutTime, completedWorkoutId } = req.body
@@ -28,6 +30,7 @@ router.post('/createSnapshot', authenticate, async (req: Request, res: Response)
 })
 
 router.get('/getSnapshot', authenticate, async (req: Request, res: Response) => {
+  const weeklySnapshot = new WeeklySnapshotsService()
   const { userId } = req.session?.passport?.user
 
   try {
@@ -38,6 +41,41 @@ router.get('/getSnapshot', authenticate, async (req: Request, res: Response) => 
   } catch (error) {
     res.status(500).json({
       message: `Something went wrong we getting weekly snapshot for userid: ${userId}`,
+      error: error,
+    })
+  }
+})
+
+router.post('/createCompletedWorkout', authenticate, async (req: Request, res: Response) => {
+  const completedWorkouts = new CompletedWorkoutsService()
+
+  try {
+    const { userId } = req.session?.passport?.user
+    const { completedWorkoutParams } = req.body
+
+    await completedWorkouts.create(userId, completedWorkoutParams)
+
+    res.sendStatus(200)
+  } catch (error: any) {
+    res.status(500).json({
+      message: 'Something went wrong creating a new completed workout.',
+      error: error.message,
+    })
+  }
+})
+
+router.get('/getCompletedWorkouts', authenticate, async (req: Request, res: Response) => {
+  const completedWorkouts = new CompletedWorkoutsService()
+  const { userId } = req.session?.passport?.user
+
+  try {
+    const completedWorkoutsData = await completedWorkouts.getCompletedWorkoutsById(userId)
+    res.status(200).json({
+      completedWorkoutsData,
+    })
+  } catch (error) {
+    res.status(500).json({
+      message: `Something went wrong we getting completed workouts for userid: ${userId}`,
       error: error,
     })
   }
